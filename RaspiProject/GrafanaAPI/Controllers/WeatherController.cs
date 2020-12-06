656bde013b634856739420382b15a493ff79cd45
Aleksi Sutela <asutela@hotmail.com>
2020-12-06 23:08:01 +0200

    Fix typo

--- RaspiProject/GrafanaAPI/Controllers/WeatherController.cs
+++ RaspiProject/GrafanaAPI/Controllers/WeatherController.cs
@@ -8,7 +8,7 @@ namespace GrafanaAPI.Controllers
     {
         public IActionResult Index()
         {
-            var html = @"<center> img src = ""http://grafana.org/assets/img/logo_new_transparent_200x48.png"" /> </center><p>Terveisiä APIsta</p>";
+            var html = @"<center> <img src=""http://grafana.org/assets/img/logo_new_transparent_200x48.png"" /> </center><p>Terveisiä APIsta</p>";
             var contentType = "text/xml";
             var content = html;
             var bytes = Encoding.UTF8.GetBytes(content);
