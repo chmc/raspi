738cf714631332bf869a3aa7fa16ef47ff9fb46b
Aleksi Sutela <asutela@hotmail.com>
2020-12-06 22:23:16 +0200

    Add WeatherController

---
+++ RaspiProject/GrafanaAPI/Controllers/WeatherController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GrafanaAPI.Controllers
+{
+    [Route("[controller]")]
+    public class WeatherController : Controller
+    {
+        public IActionResult Index()
+        {
+            var html = @"<center> img src = ""http://grafana.org/assets/img/logo_new_transparent_200x48.png"" /> </center><p>Terveisiä APIsta</p>";
+            return new OkObjectResult(html);
+        }
+    }
+}
